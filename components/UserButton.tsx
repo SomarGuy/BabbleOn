668d0500afcd1934beda35e059caac30cf5b1004
SomarGuy <diogo.ramos@me.com>
2023-11-08 17:06:52 -0500

    Start building the User Avatar Component

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -6,11 +6,14 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
+import UserAvatar from "./UserAvatar"
   
 const UserButton = () => {
   return (
     <DropdownMenu>
-  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuTrigger>
+    <UserAvatar name="Diogo Ramos" image='https://github.com/shadcn.png' />
+  </DropdownMenuTrigger>
   <DropdownMenuContent>
     <DropdownMenuLabel>My Account</DropdownMenuLabel>
     <DropdownMenuSeparator />
