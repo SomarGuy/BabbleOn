56fa9351f3829cd99d176b120a4cc7840ecf51dd
SomarGuy <diogo.ramos@me.com>
2023-11-08 16:39:55 -0500

    Start building the User button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from './Logo'
 import DarkModeToggle  from './DarkModeToggle'
+import UserButton from './UserButton'
 const Header = () => {
   return <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
     <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto'>
@@ -9,6 +10,8 @@ const Header = () => {
         <div className='flex-1 flex items-center justify-end space-x-4'>
 
           <DarkModeToggle />
+
+          <UserButton />
         </div>
     </nav>
   </header>
