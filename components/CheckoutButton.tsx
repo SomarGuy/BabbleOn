e82edf50b460f34bfdad0378a2b496ef3f8dfb8c
SomarGuy <diogo.ramos@me.com>
2023-11-12 18:44:28 -0500

    build the checkout button component

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+'use client';
 
-const CheckoutButton = () => {
-  return (
-    <div>CheckoutButton</div>
-  )
+import { useSession } from "next-auth/react";
+
+function CheckoutButton() {
+  const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    if (!session) return;
+  }
+
+  return ( 
+    <div className="flex flex-col space-y-2">
+      <button onClick={() => createCheckoutSession()} className="mt-8 block rounded-md bg-blue-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 cursor-pointer disabled:opacity-80 disabled:bg-blue-600/50 disabled:text-white   disabled:cursor-default"
+  >
+        Sign Up
+      </button>;
+    </div>
+  );
 }
 
 export default CheckoutButton
