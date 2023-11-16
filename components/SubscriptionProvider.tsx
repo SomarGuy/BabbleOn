930aaa779ef71d3dddf45389250050a20d390fd4
SomarGuy <diogo.ramos@me.com>
2023-11-16 17:05:27 -0500

    set up type definitions

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -1,11 +1,22 @@
 'use client'
+import { subscriptionRef } from "@/lib/converters/Subscription";
+import { onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 function SubscriptionProvider() {
     const { data: session } = useSession();
 
-    useEffect(() => {}, []);
+    useEffect(() => {
+        if (!session) return;
+
+        return onSnapshot(subscriptionRef(session?.user.id), (snapshot) => {
+            if (snapshot.empty) {
+                console.log("User has NO subscription");
+                return;
+            }
+        })
+    }, [session]);
 
     return <div>SubscriptionProvider</div>;
 }
