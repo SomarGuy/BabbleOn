d12d5a1aef2c0e321a6f089871597ec3afab54bb
SomarGuy <diogo.ramos@me.com>
2023-11-16 17:42:58 -0500

    add loading spinner

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -4,6 +4,7 @@ import { db } from "@/firebase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 function CheckoutButton() {
   const { data: session } = useSession();
@@ -43,7 +44,7 @@ function CheckoutButton() {
     <div className="flex flex-col space-y-2">
       <button onClick={() => createCheckoutSession()} className="mt-8 block rounded-md bg-blue-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 cursor-pointer disabled:opacity-80 disabled:bg-blue-600/50 disabled:text-white   disabled:cursor-default"
   >
-        {loading ? 'loading...' : "Sign Up"}
+        {loading ? <LoadingSpinner /> : "Sign Up"}
       </button>;
     </div>
   );
