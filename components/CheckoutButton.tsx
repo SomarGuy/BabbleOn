b4d5946338d475cb9a888eeb75d705d0f6019c6b
SomarGuy <diogo.ramos@me.com>
2023-11-16 16:53:48 -0500

    build the checkout button component

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { db } from "@/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
@@ -15,9 +15,25 @@ function CheckoutButton() {
     setLoading(true)
 
     const docRef = await addDoc(collection(db, 'customers', session.user.id, 'checkout_sessions'), {
-      price: "price_100998KDjTc6FlwiIL3eBoYQ",
+      price: "price_1OCvG6EPlgP4a8WnbafFnuNH",
       success_url: window.location.origin,
       cancel_url: window.location.origin,
+    })
+
+    return onSnapshot(docRef, snap => {
+      const data = snap.data();
+      const url = data?.url;
+      const error = data?.error;
+
+      if (error) {
+        alert(`An error occured: ${error.message}`);
+        setLoading(false);
+      }
+
+      if (url) {
+        window.location.assign(url);
+        setLoading(false);
+      }
     })
 
     
@@ -27,7 +43,7 @@ function CheckoutButton() {
     <div className="flex flex-col space-y-2">
       <button onClick={() => createCheckoutSession()} className="mt-8 block rounded-md bg-blue-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 cursor-pointer disabled:opacity-80 disabled:bg-blue-600/50 disabled:text-white   disabled:cursor-default"
   >
-        Sign Up
+        {loading ? 'loading...' : "Sign Up"}
       </button>;
     </div>
   );
