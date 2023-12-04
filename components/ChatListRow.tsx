d7136e155b3dea64e0b48b51e996d563d8918d0e
SomarGuy <diogo.ramos@me.com>
2023-12-03 21:10:30 -0500

    implement a skeleton loader for chatlist

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -1,6 +1,12 @@
 'use client';
+import { Skeleton } from "@/components/ui/skeleton";
+import { Message, limitedSortedMessagesRef } from "@/lib/converters/Message";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 function ChatListRow({chatId} : {chatId: string}) {
+    const [messages, loading, error] = useCollectionData<Message>(
+        limitedSortedMessagesRef(chatId)
+    )
   return (
     <div>ChatListRow</div>
   )
