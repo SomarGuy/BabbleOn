5163c4a9f0fe8a947993152090c553e4cbb97fdb
SomarGuy <diogo.ramos@me.com>
2023-12-04 17:39:27 -0500

    build chat input with zodd

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,8 +1,19 @@
+import { authOptions } from "@/auth"
+import ChatInput from "@/components/ChatInput"
+import { getServerSession } from "next-auth"
 
-function ChatPage() {
+type Props = {
+  params: {
+    chatId: string;
+  }
+}
+
+async function ChatPage({ params: { chatId } }: Props) {
+  const session = await getServerSession(authOptions)
   return (
     <>
     
+    <ChatInput chatId={chatId}/>
     </>
   )
 }
